f827be203201b8d4d6bb581bac6f3ad263116e29
Clint <clintp1@gmail.com>
2019-07-26 11:24:30 +1000

    refactor ios background-image url to inline svg to fix issue when inside pwa apps

--- morph-radio-ios-css.js
+++ morph-radio-ios-css.js
@@ -1,7 +1,8 @@
 import { html } from 'lit-element';
 
-const checkmarkSvg = "data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 13 10'><polygon fill='#007aff' points='11.6,0 4.4,7.2 1.4,4.2 0,5.6 4.4,10 4.4,10 4.4,10 13,1.4' /></svg>";
+const checkmarkSvg = "data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 13 10'><polygon fill='#d02e3d' points='11.6,0 4.4,7.2 1.4,4.2 0,5.6 4.4,10 4.4,10 4.4,10 13,1.4' /></svg>";
 
+const checkmarkSvg64 = "data:image/svg+xml;base64, PHN2ZyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIHZpZXdCb3g9JzAgMCAxMyAxMCc+PHBvbHlnb24gZmlsbD0nI2QwMmUzZCcgcG9pbnRzPScxMS42LDAgNC40LDcuMiAxLjQsNC4yIDAsNS42IDQuNCwxMCA0LjQsMTAgNC40LDEwIDEzLDEuNCcgLz48L3N2Zz4="
 export const morphRadioIosStyle = html`
   <style>
     .radio {
@@ -28,7 +29,7 @@ export const morphRadioIosStyle = html`
     .radio > input[type=radio]:checked + .icon {
       border-color: white;
       background: no-repeat center;
-      background-image: url('/src/images/ios-checkmark.svg');
+      background-image: url("${checkmarkSvg64}");
       background-size: 13px 10px;
     }
 
